ad7828d33606a0a337605ce8bec97af9fd781475
krishdb38 <45994360+krishdb38@users.noreply.github.com>
2020-04-14 16:37:16 +0900

    14

--- tkinter_pass_arg_button.py
+++ tkinter_pass_arg_button.py
@@ -6,8 +6,14 @@ def openURL(x):
     new =2
     webbrowser.open(x,new = new)
 
+
 # From Setup
 window = tk.Tk()
 window.geometry("350x350")
 window.resizable(0,0)
-window.title("")
+window.title("Web Application")
+
+# button
+button = tk.Button(window, text = "Google Search",width = 25,command = lambda: openURL("https://www.google.com/"))
+button.pack()
+window.mainloop()
